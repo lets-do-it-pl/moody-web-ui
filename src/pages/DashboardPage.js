b078838e111b29acf7d3c70244dfb38703595be3
Elif Aygun <elifdilaa@gmail.com>
2020-10-18 14:02:01 +0200

    Template is modified for user's requests

---
+++ src/pages/DashboardPage.js
@@ -0,0 +1,24 @@
+import Page from 'components/Page';
+import React from 'react';
+
+class DashboardPage extends React.Component {
+  componentDidMount() {
+    // this is needed, because InfiniteCalendar forces window scroll
+    window.scrollTo(0, 0);
+  }
+
+  render() {
+    
+    return (
+      <Page
+        className="DashboardPage"
+        title="Dashboard"
+        breadcrumbs={[{ name: 'Dashboard', active: true }]}
+      >
+        
+
+      </Page>
+    );
+  }
+}
+export default DashboardPage;
---
+++ src/pages/FormPage.js
@@ -0,0 +1,26 @@
+import Page from 'components/Page';
+import React from 'react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Col,
+  Form,
+  FormFeedback,
+  FormGroup,
+  FormText,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap';
+
+const FormPage = () => {
+  return (
+    <Page title="Forms" breadcrumbs={[{ name: 'Forms', active: true }]}>
+      
+    </Page>
+  );
+};
+
+export default FormPage;
