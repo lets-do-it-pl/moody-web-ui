e4b8412ebc208c49c93f2fc22549c8ee6e26d25b
Hamdi Özdil <mh.ozdil6@gmail.com>
2020-11-01 16:23:57 +0100

    Removing test components on DashboardPage
    Because of problems on ProfilePage, codes were in Dashboard. After solving the problem on ProfilePage no needed these items anymore

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,12 +1,5 @@
 import Page from 'components/Page';
 import React from 'react';
-import ProfileCard from 'components/Card/ProfileCard';
-import InfoCard from 'components/Card/InfoCard';
-
-import {
-  Col,
-  Row,
-} from 'reactstrap';
 
 class DashboardPage extends React.Component {
   componentDidMount() {
@@ -21,31 +14,7 @@ class DashboardPage extends React.Component {
         className="DashboardPage"
         title="Dashboard"
         breadcrumbs={[{ name: 'Dashboard', active: true }]}
-      >
-        <Row>
-          <Col lg="6" md="12" sm="12" xs="12">
-            <ProfileCard
-              color="gradient-white"
-              avatarSize={400}
-              style={{ height: 500 }}
-            />
-          </Col>
-
-          <Col lg="6" md="12" sm="12" xs="12">
-            <InfoCard
-              color="gradient-white"
-              header="Information"
-              fullname="Başkan Dayı"
-              email="blabla@deneme.com"
-              buttonProps={{
-                children: 'Save',
-              }}
-              style={{ height: 500 }}
-            />
-          </Col>
-
-        </Row>
-        
+      >       
 
       </Page>
     );
