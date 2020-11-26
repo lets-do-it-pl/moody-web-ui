7f4f03bbc29a0f9055d6c36a5df346eec37b9fe8
NotMuchLeft <ahmetkuris2308@gmail.com>
2020-11-26 08:43:36 +0100

    Add SignUpPage

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,5 +1,4 @@
 import logo200Image from 'assets/img/logo/logo_200.png';
-import PropTypes from 'prop-types';
 import React from 'react';
 import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import  Recaptcha from 'react-recaptcha';
