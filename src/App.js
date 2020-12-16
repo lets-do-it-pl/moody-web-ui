06d290cbfd8d75ef9e690db18719e4d39076008e
Saban Durna <sabandurna@gmail.com>
2020-12-16 10:03:24 +0100

    Move new theme

--- src/App.js
+++ src/App.js
@@ -1,89 +1,21 @@
+import 'react-perfect-scrollbar/dist/css/styles.css';
 import React from 'react';
-import GAListener from 'components/GAListener';
-import { MainLayout, EmptyLayout } from 'components/Layout';
-import PageSpinner from 'components/PageSpinner';
-import componentQueries from 'react-component-queries';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
-import './styles/moody.scss';
-import 'react-notifications/lib/notifications.css';
-import NotificationContainer from 'react-notifications';
-import UserService from './services/user.service';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/core';
+import GlobalStyles from 'src/components/GlobalStyles';
+import 'src/mixins/chartjs';
+import theme from 'src/theme';
+import routes from 'src/routes';
 
-const AuthPage = React.lazy(() => import('./pages/AuthPage'));
-const DashboardPage = React.lazy(() => import('pages/DashboardPage'));
+const App = () => {
+  const routing = useRoutes(routes);
 
-const getBasename = () => {
-  return `/${process.env.PUBLIC_URL.split('/').pop()}`;
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      {routing}
+    </ThemeProvider>
+  );
 };
 
-class App extends React.Component {
-
-  state = {
-    isAuthed: false
-  };
-
-  componentDidMount() {
-    this.checkAuth();
-  }
-
-  checkAuth = () => {
-    var isAuthed = UserService.isUserLoggedIn();
-
-    this.setState({
-      isAuthed: isAuthed
-    });
-  }
-
-  render() {
-    return (
-      <BrowserRouter basename={getBasename()}>
-        <GAListener>
-          <Switch>
-            <EmptyLayout>
-              <React.Suspense fallback={<PageSpinner />}>
-                <Route path="/auth" component={AuthPage} />
-              </React.Suspense>
-            </EmptyLayout>
-            <MainLayout>
-              <React.Suspense fallback={<PageSpinner />}>
-                <Route path="/" component={DashboardPage} />
-              </React.Suspense>
-            </MainLayout>
-            {this.state.isAuthed ?
-              <Redirect to="/" /> :
-              <Redirect to="/auth" />
-            }
-            <NotificationContainer />
-
-          </Switch>
-        </GAListener>
-      </BrowserRouter>
-    );
-  }
-}
-
-const query = ({ width }) => {
-  if (width < 575) {
-    return { breakpoint: 'xs' };
-  }
-
-  if (576 < width && width < 767) {
-    return { breakpoint: 'sm' };
-  }
-
-  if (768 < width && width < 991) {
-    return { breakpoint: 'md' };
-  }
-
-  if (992 < width && width < 1199) {
-    return { breakpoint: 'lg' };
-  }
-
-  if (width > 1200) {
-    return { breakpoint: 'xl' };
-  }
-
-  return { breakpoint: 'xs' };
-};
-
-export default componentQueries(query)(App);
+export default App;
--- src/components/Card/index.js
+++
@@ -1,2 +0,0 @@
-export { default as UserCard } from './UserCard';
-
