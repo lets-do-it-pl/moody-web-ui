1180342902059a744243a2955824e167e1261e4e
Meryem Sel <mzbasel637936@gmail.com>
2021-03-11 20:26:09 +0100

    MOODY-120-Requested changes has been implemented

--- src/views/settings/RemoveCache.js
+++ src/views/settings/RemoveCache.js
@@ -15,7 +15,15 @@ import { categoryService } from '../../_services/category.service';
 const useStyles = makeStyles((theme) => ({
   removeButton : {
     backgroundColor: "#f44336",
-    color: "white"
+    color: "white",
+    marginBottom: '5px'
+  },
+  info : {
+    fontSize: 16,
+    fontFamily: 'BlinkMacSystemFont',
+  },
+  warning : {
+    color: "#f44336"
   }
 }));
 
@@ -53,17 +61,20 @@ function RemoveCache() {
   return (
     <Card>
     <CardHeader
-      title="Remove Cache Key"
+      title="Refresh Category Version Cache"
     />
     <Divider />
     <CardContent>
-    <Button
-          variant = "contained"
-          className = {classes.removeButton}
-          onClick = {removeCache}
-        >
-          Remove Cache Key
-        </Button>
+      <Button
+            variant = "contained"
+            className = {classes.removeButton}
+            onClick = {removeCache}>
+          Refresh Version Cache
+      </Button>
+      <Divider/>
+      <h3 className = {classes.info}> <h3 className = {classes.warning}>Info:</h3> This button removes the cache key so you can update the cached 
+      version number to current version number. Normally we have to wait 24 hours in order to cache to refresh itself
+       but by clicking you may refresh it without waiting that remaining time.</h3>
     </CardContent>
   </Card>
   )
