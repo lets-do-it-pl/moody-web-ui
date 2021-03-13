bf00c716d22c5f52a67ed5828acb4b88398ebedd
yzrSamet <yzr.samet@gmail.com>
2021-03-13 23:39:55 +0100

    MOODY-55-implement-account-view-page Add upload Image Functionality without Post Method

--- src/views/account/AccountView/Profile.js
+++ src/views/account/AccountView/Profile.js
@@ -1,5 +1,6 @@
+import ImageUploader from 'react-images-upload'
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import React from 'react';
 import clsx from 'clsx';
 import {
   Avatar,
@@ -16,25 +17,46 @@ import {
 const useStyles = makeStyles(() => ({
   root: {},
    avatar: {
-    height: 100,
-    width: 100
+    height: 200,
+    width: 200
+  },
+  ImageUploader :{
+    marginTop : 2,
+    marginBottom :2
   }
 }));
 
-const Profile = (props) => {
-  
-  const {account, className, ...rest } = props;
-  
-  const userAccount = {
-    avatar: account.image,
-    name: account.fullName
-  };
+const convertBase64 = (image) => {
+  return new Promise((resolve, reject) => {
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(image);
 
+    fileReader.onload = () => {
+      resolve(fileReader.result);
+    };
+
+    fileReader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+
+function Profile (props) {
   const classes = useStyles();
+  const [name, setName] = useState(props.account.name);
+  const [image, setImage] = useState(props.account.image);
+  
+  const fileUploadHandler = async (file) =>{
+    const base64 = await convertBase64(file[0]);
+    const base = base64.split(/[,]+/);
+    setImage(base[1]);
+  
+  }
+
   return (
+    
     <Card
-      className={clsx(classes.root, className)}
-      {...rest}
+      className={clsx(classes.root, name)}
     >
       <CardContent>
         <Box
@@ -44,28 +66,36 @@ const Profile = (props) => {
         >
           <Avatar
             className={classes.avatar}
-            src={"data:image/png;base64," + userAccount.avatar}
+            src={"data:image/png;base64," + image}
           />
           <Typography
             color="textPrimary"
             gutterBottom
             variant="h3"
           >
-            {userAccount.name}
+            {name}
           </Typography>
 
         </Box>
       </CardContent>
       <Divider />
       <CardActions>
-        <Button
-          color="primary"
-          fullWidth
-          variant="text"
-        >
-          Upload picture
-        </Button>
-      </CardActions>
+        
+        <ImageUploader
+            {...props}
+            id="image-upload"
+            name="image"
+            withLabel={false}
+            withIcon ={false}
+            singleImage={true}
+            withPreview={false}
+            buttonText= 'Upload Image'
+            onChange= {fileUploadHandler}
+            imgExtension={['.jpg', '.jpeg', '.png', '.gif']}
+            maxFileSize={5242880}
+            
+        ></ImageUploader>
+      </CardActions>  
     </Card>
   );
 };
